a78a77301e6692aecd5666be3be667a686b9aea4
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-12 11:44:11 +0700

    ch11 fixed position

--- src/component/Section1Component.jsx
+++ src/component/Section1Component.jsx
@@ -8,8 +8,8 @@ const Section1Component = () => {
         <img src={interactive_desktop} alt="" className=" hidden lg:block "/>
         <img src={interactive_mobile} alt="" className=" w-full lg:hidden" />
 
-        <div className=" static top-0 left-0 bg-white    pt-[2rem] px-[1rem]  text-center
-                        lg:absolute lg:top-[40%] lg:bottom-0 lg:left-[50%]   lg:w-[600px]  lg:text-start lg:px-[4.8rem] lg:pt-[4.8rem]">
+        <div className=" static top-0  bg-white    pt-[2rem] px-[1rem]  text-center
+                        lg:absolute lg:top-[40%] lg:bottom-0 lg:right-0  lg:w-[600px]  lg:text-start lg:px-[4.8rem] lg:pt-[4.8rem]">
           <h1 className=" font-Josefin text-[2rem] font-medium uppercase leading-[2rem] mt-[1rem]
                           lg:text-[3rem] lg:leading-[3rem]">The leader in interactive VR</h1>
           <p className="text-[15px] text-[#8c8c8c] font-medium mt-[1rem]">Founded in 2011, Loopstudios has been producing world-class virtual reality 
