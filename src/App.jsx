a380b586ff15e9091775d9c1780bf86e1d274942
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-12 12:15:26 +0700

    ch11 import MainCOmponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import HeaderComponent from "./component/HeaderComponent"
+import MainComponent from "./component/MainComponent"
 
 
 function App() {
 
+  
   return (
     <>
     <HeaderComponent/>
+    <MainComponent/>
     </>
   )
 }
