74fee5013335ad92425ae3a63b9c8ece3f77d626
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-12 15:16:09 +0700

    ch11 import Section2Component

--- src/component/MainComponent.jsx
+++ src/component/MainComponent.jsx
@@ -1,4 +1,5 @@
 import Section1Component from "./Section1Component"
+import Section2Component from "./Section2Component"
 
 
 
@@ -8,6 +9,7 @@ const MainComponent = () => {
     <main className="px-[1.5rem] py-[6rem]
                     lg:px-[10rem] lg:py-[10rem]">
         <Section1Component/>
+        <Section2Component/>
     </main>
   )
 }
