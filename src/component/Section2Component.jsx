81d3daf6cd9ea0d38f7846293871affd0455cd92
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-13 12:33:13 +0700

    ch11 use state when select image& add padding obj

--- src/component/Section2Component.jsx
+++ src/component/Section2Component.jsx
@@ -14,9 +14,12 @@ import curiosity_mobile from "../images/mobile/image-curiosity.jpg";
 import curiosity_desktop from "../images/desktop/image-curiosity.jpg";
 import fisheye_mobile from "../images/mobile/image-fisheye.jpg";
 import fisheye_desktop from "../images/desktop/image-fisheye.jpg";
+import { useState } from "react";
 
 
 const Section2Component = () => {
+
+    const [imageclick , setImageclick] = useState('')
 
     const card = [{
         image_mobile: deep_mobile,
@@ -32,7 +35,7 @@ const Section2Component = () => {
     {   image_mobile: soccer_mobile,
         image_desktop:soccer_dektop,
         title:'Soccer team VR',
-        paddingr:'pr-[50%]'
+        paddingr:'pr-[50%] lg:pr-[20%]'
     },
     {   image_mobile: grid_mobile,
         image_desktop:grid_desktop,
@@ -42,7 +45,7 @@ const Section2Component = () => {
     {   image_mobile: above_mobile,
         image_desktop:above_desktop,
         title:'From up above VR',
-        paddingr:'pr-[50%]'
+        paddingr:'pr-[50%] lg:pr-[20%]'
     },
     {   image_mobile: borealis_mobile,
         image_desktop:borealis_desktop,
@@ -58,9 +61,12 @@ const Section2Component = () => {
         image_desktop:fisheye_desktop,
         title:'Make it fisheye',
         paddingr:'pr-[50%]'
+    }]
+
+    const SelectImage =(event)=>{
+        setImageclick(event.target.id)
     }
-    
-    ]
+
   return (
         <section    className="py-[8rem] lg:py-[9rem]">
             <div className="flex flex-col lg:relative ">
@@ -72,23 +78,26 @@ const Section2Component = () => {
                 <div className="flex flex-wrap gap-x-0 gap-y-[1rem] w-full
                                 lg:gap-x-[2rem] lg:gap-y-[2rem] ">
                     {card.map((e,index)=>{
-                        return <span key={index} className=" relative w-full lg:w-auto" >
+                        return  <section key={index} className=" relative w-full lg:w-auto" >
                                     <div className=" relative">
-                                        <img src={e.image_desktop} alt="" className="hidden  lg:block" />
-                                        <img src={e.image_mobile} alt="" className=" block w-full  lg:hidden" />
-                                        <div className=" absolute top-0 left-0 h-full w-[60%]  bg-gradient-to-r from-[#000000c5] to-transparent"></div>
+                                        <img src={e.image_desktop} alt={e.image_desktop} className={imageclick == e.title   ?'hidden  lg:block opacity-[0.4]'
+                                                                                                                            :'hidden  lg:block'}/>
+                                        <img src={e.image_mobile} alt={e.image_mobile} className={imageclick == e.title     ?'block w-full opacity-[0.4]   lg:hidden'
+                                                                                                                            :'block w-full   lg:hidden'} />
+                                        <div className={imageclick == e.title   ?'hidden '
+                                                                                :'absolute top-0 left-0 h-full w-[60%]  bg-gradient-to-r from-[#000000c5] to-transparent'}>
+                                        </div>
                                     </div>                                    
-                                    <h1 className={`uppercase absolute bottom-[1rem] left-[1rem] text-white text-[1.5rem] font-Josefin  ${e.paddingr} leading-[1.5rem]
-                                                    lg:bottom-[2rem] lg:left-[2rem] lg:text-[2.1rem] lg:leading-[2.1rem]`}>{e.title}
+                                    <h1 className={`${imageclick == e.title ?` text-black title-section2  ${e.paddingr}`       :`text-white title-section2 ${e.paddingr}`}`}>{e.title}
                                     </h1>
-                                </span>
+                                    <div className={`  absolute top-0 left-0 h-full w-full hover:cursor-pointer`} id={`${e.title}`} onClick={SelectImage}></div>
+                                </section>
                     })}
                 </div>
                 <div className="flex justify-center lg:absolute lg:top-0 lg:right-0 lg:block lg:justify-normal">
                     <button className=" font-Alata uppercase tracking-[0.4rem] mt-[2rem] border border-black px-[2.5rem] py-[0.6rem] 
                                         lg:mt-0
-                                        hover:bg-black hover:text-white
-                                        active:bg-black active:text-white">
+                                        hover:bg-black hover:text-white">
                         See all
                     </button>
                 </div>
